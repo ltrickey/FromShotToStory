fd961cfc09cc4a12a25be550531279565a1792cc
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-05 12:31:55 -0700

    Added content for default cells in table view

---
+++ MoviePracticeApp/MoviePracticeApp/ShotTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShotTableViewCell.swift
+//  MoviePracticeApp
+//
+//  Created by Lynn Trickey on 7/5/17.
+//  Copyright © 2017 Lynn Trickey. All rights reserved.
+//
+
+import UIKit
+
+class ShotTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
