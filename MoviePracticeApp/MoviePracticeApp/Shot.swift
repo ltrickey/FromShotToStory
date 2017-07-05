4b6d9d4c442d0892332e6f90c8143199e386baa2
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-05 11:56:46 -0700

    Set up properties and initialization func for Shot class

--- MoviePracticeApp/MoviePracticeApp/Shot.swift
+++ MoviePracticeApp/MoviePracticeApp/Shot.swift
@@ -7,3 +7,26 @@
 //
 
 import UIKit
+
+class Shot {
+    
+    //MARK: Properties
+    // Making them constants b/c shouldn't be changed by kids.
+    
+    let name: String
+    let photo: UIImage?
+    let video: String? // using Youtube API?  String or no?
+    let description: String
+    
+    
+    //MARK: Initialization
+    
+    init(name: String, photo: UIImage?, video: String?, description: String) {
+        
+        self.name = name
+        self.photo = photo
+        self.video = video
+        self.description = description
+    }
+    
+}
