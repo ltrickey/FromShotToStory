d11baadc27b51fe697d36ad5c78e03abd437b921
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-06 11:09:40 -0700

    Fixed table view auto constraints

--- MoviePracticeApp/MoviePracticeApp/ShotTableViewCell.swift
+++ MoviePracticeApp/MoviePracticeApp/ShotTableViewCell.swift
@@ -15,16 +15,15 @@ class ShotTableViewCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+//        photoImageView.frame =  CGRect(x: 0.0, y: 0.0, width: 100, height: 100 )
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
