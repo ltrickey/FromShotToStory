3046d2f5607e62a5adb4f98d68db868abdcb6545
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-08 11:49:38 -0700

    Figured out how to add this code as a submodule

---
+++ MoviePracticeApp/MoviePracticeApp/VideoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VideoViewController.swift
+//  MoviePracticeApp
+//
+//  Created by Lynn Trickey on 7/6/17.
+//  Copyright © 2017 Lynn Trickey. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+import YouTubePlayer
+
+class VideoViewController: UIViewController {
+
+    @IBOutlet weak var youtubePlayer: YouTubePlayerView!
+}
