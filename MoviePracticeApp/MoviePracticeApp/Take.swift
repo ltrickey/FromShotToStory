ba6bc7583532e04ec2a4ffc8068ac9a7517be8ed
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-11 15:40:09 -0700

    Created class Take

---
+++ MoviePracticeApp/MoviePracticeApp/Take.swift
@@ -0,0 +1,27 @@
+//
+//  Take.swift
+//  MoviePracticeApp
+//
+//  Created by Lynn Trickey on 7/11/17.
+//  Copyright © 2017 Lynn Trickey. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class Take {
+    
+    //MARK: Properties
+    let url: URL
+    let thumbnail: UIImage
+    
+    //MARK: initialization
+    
+    init(url: URL, thumbnail: UIImage?) {
+        
+        self.url = url
+        self.thumbnail = thumbnail!
+    }
+    
+}
