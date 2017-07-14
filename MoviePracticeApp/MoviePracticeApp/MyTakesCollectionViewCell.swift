08671f14cd6e7f45cd66ae29b9bd59b72c8a6680
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-14 14:21:00 -0700

    Added the delete button in each cell from ib outlet instead of programatically

--- MoviePracticeApp/MoviePracticeApp/MyTakesCollectionViewCell.swift
+++ MoviePracticeApp/MoviePracticeApp/MyTakesCollectionViewCell.swift
@@ -15,18 +15,18 @@ class MyTakesCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var savedShotImageView: UIImageView!
     
-    var deleteButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        // Create a UIButton
-        deleteButton = UIButton(frame: CGRect(x: frame.size.width/10, y: frame.size.width/16, width: frame.size.width/4, height: frame.size.width/4))
-        
-//        // Set the UIButton's image property
-//        deleteButtonImg = UIImage(named: "delete-icon")!
-//        deleteButton.setImage(deleteButtonImg, forState: UIControlState.Normal)
-        
+//        // Create a UIButton
+//        deleteButton = UIButton(frame: CGRect(x: frame.size.width/10, y: frame.size.width/16, width: frame.size.width/4, height: frame.size.width/4))
+//        
+////        // Set the UIButton's image property
+////        deleteButtonImg = UIImage(named: "delete-icon")!
+////        deleteButton.setImage(deleteButtonImg, forState: UIControlState.Normal)
+//        
         // Add the UIButton to the collection view
         contentView.addSubview(deleteButton)
 
