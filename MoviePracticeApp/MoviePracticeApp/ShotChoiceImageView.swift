71a7ef683eadc210e827ee7dcd1dadf7daf551b3
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-18 13:53:07 -0700

    Added new uiview controller with four shotchoiceimageview image views

---
+++ MoviePracticeApp/MoviePracticeApp/ShotChoiceImageView.swift
@@ -0,0 +1,21 @@
+//
+//  ShotChoiceImageView.swift
+//  MoviePracticeApp
+//
+//  Created by Lynn Trickey on 7/18/17.
+//  Copyright © 2017 Lynn Trickey. All rights reserved.
+//
+
+import UIKit
+
+class ShotChoiceImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MoviePracticeApp/MoviePracticeApp/StoryBoardViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StoryBoardViewController.swift
+//  MoviePracticeApp
+//
+//  Created by Lynn Trickey on 7/18/17.
+//  Copyright © 2017 Lynn Trickey. All rights reserved.
+//
+
+import UIKit
+
+class StoryBoardViewController: UIViewController {
+
+    @IBOutlet weak var FirstShot: UIImageView!
+    @IBOutlet weak var SecondShot: UIImageView!
+    @IBOutlet weak var ThirdShot: UIImageView!
+    @IBOutlet weak var FourthShot: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
