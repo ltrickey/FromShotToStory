aaefc6d1bb38224ad43a52551ca48aa47697d9fc
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-19 13:17:49 -0700

    got story drop down working!

--- MoviePracticeApp/MoviePracticeApp/StoryViewController.swift
+++ MoviePracticeApp/MoviePracticeApp/StoryViewController.swift
@@ -9,30 +9,27 @@
 import UIKit
 import DropDown
 
-class StoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+class StoryViewController: UIViewController, UIPickerViewDelegate {
     
     let shots = ["Close Up", "Establishing"]
     let images = ["closeUpShot", "establishing"]
 
     @IBOutlet weak var shotTypesCollectionView: UICollectionView!
     
-    @IBOutlet weak var selectStoryTextField: UITextField!
-    
-    @IBOutlet weak var selectStoryPicker: UIPickerView!
-    
     @IBOutlet weak var selectShotsLabel: UILabel!
     @IBOutlet weak var selectShotsExample: UILabel!
     @IBOutlet weak var selectShotsEncouragement: UILabel!
     
+    @IBOutlet weak var storyToTryButton: UIBarButtonItem!
     
     @IBOutlet weak var firstShotDropDown: UIBarButtonItem!
     @IBOutlet weak var firstShotImageView: UIImageView!
     
     let firstShotDropDownMenu = DropDown()
-    
+    let storyDropDown = DropDown()
 
     
-    var list = ["", "Jess is having a terrible day.", "Dustin is enjoying the beautiful weather.", "Lila gets distracted.","A new school is very scary", "Sleep is my favorite activity", "Julia is trying to impress her teacher so she can get an A in class.", "Dylan can’t wait for school to be over so he can go to Disneyland."]
+    var stories = ["", "Jess is having a terrible day.", "Dustin is enjoying the beautiful weather.", "Lila gets distracted.","A new school is very scary", "Sleep is my favorite activity", "Julia is trying to impress her teacher so she can get an A in class.", "Dylan can’t wait for school to be over so he can go to Disneyland."]
     
     var examples = ["", "An insert shot of Jess's foot stepping in some mud.", "A tilt shot from the beautiful sky to down to Dustin's smiling face to show him enjohing the weather.", "A Point of View Shot of point of what is distracting Lila.", "A tracking shot of someone walking down the hall at a new school.", "A close up of someone not being able to keep their eyes open.",  "An over the shoulder shot as Julia turns in her paper to her teacher.", "An extreme close up of dylan's eyes as he watches the clock."]
 
@@ -40,7 +37,6 @@ class StoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         super.viewDidLoad()
 
         // story stuff
-        self.selectStoryPicker.isHidden = true
         self.selectShotsLabel.isHidden = true
         self.selectShotsExample.isHidden = true
         self.selectShotsEncouragement.isHidden = true
@@ -48,7 +44,7 @@ class StoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         //shot view stuff
 
         self.firstShotImageView.isHidden = true
-        
+        setupStoryDropDownMenu()
         setupfirstShotDropDownMenu()
         
 
@@ -65,64 +61,87 @@ class StoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     // Actions for drop downs
     @IBAction func openFirstDropDown(_ sender: Any) {
-//        if firstShotDropDownMenu.
         firstShotDropDownMenu.show()
-//        let selection = firstShotDropDownMenu.selectedItem
+
+    }
+ 
+    @IBAction func openStoryDropDown(_ sender: Any) {
+        storyDropDown.show()
     }
     
 
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        
-        if pickerView == selectStoryPicker {
-           return list.count
-        } else {
-        return shots.count
-        }
-    }
+//    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+//        
+//        if pickerView == selectStoryPicker {
+//           return list.count
+//        } else {
+//        return shots.count
+//        }
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        self.view.endEditing(true)
+//        
+//        if pickerView == selectStoryPicker {
+//            return list[row]
+//        } else {
+//            return shots[row]
+//        }
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        
+//        if pickerView == selectStoryPicker {
+//            self.selectStoryTextField.text = self.list[row]
+//            self.selectStoryPicker.isHidden = true
+//            
+//            if row != 0 {
+//                self.selectShotsLabel.isHidden = false
+//                self.selectShotsExample.isHidden = false
+//                self.selectShotsEncouragement.isHidden = false
+//                self.selectShotsExample.text = examples[row]
+//
+//            }
+//        } else {
+//            self.firstShotImageView.image = UIImage(named: images[row])
+//        }
+//        
+//    }
+//    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        if textField == self.selectStoryTextField {
+//            self.selectStoryPicker.isHidden = false
+//            self.selectShotsLabel.isHidden = true
+//            self.selectShotsExample.isHidden = true
+//            //if you dont want the users to se the keyboard type:
+//            
+//            textField.endEditing(true)
+//            
+//        }
+//        
+//    }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        self.view.endEditing(true)
-        
-        if pickerView == selectStoryPicker {
-            return list[row]
-        } else {
-            return shots[row]
-        }
-    }
+    //Setup Drop downs
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    func setupStoryDropDownMenu() {
+        // The view to which the drop down will appear on
         
-        if pickerView == selectStoryPicker {
-            self.selectStoryTextField.text = self.list[row]
-            self.selectStoryPicker.isHidden = true
-            
-            if row != 0 {
-                self.selectShotsLabel.isHidden = false
-                self.selectShotsExample.isHidden = false
-                self.selectShotsEncouragement.isHidden = false
-                self.selectShotsExample.text = examples[row]
+        storyDropDown.anchorView = storyToTryButton as UIBarButtonItem
+        
+        // The list of items to display. Can be changed dynamically
+        storyDropDown.dataSource = self.stories
+        
+        // Action triggered on selection
+        storyDropDown.selectionAction = { [unowned self] (index, item) in
+            self.storyToTryButton.title = item
+            self.selectShotsExample.text = self.examples[index]
 
-            }
-        } else {
-            self.firstShotImageView.image = UIImage(named: images[row])
+            self.selectShotsLabel.isHidden = false
+            self.selectShotsExample.isHidden = false
+            self.selectShotsEncouragement.isHidden = false
         }
-        
+       
     }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == self.selectStoryTextField {
-            self.selectStoryPicker.isHidden = false
-            self.selectShotsLabel.isHidden = true
-            self.selectShotsExample.isHidden = true
-            //if you dont want the users to se the keyboard type:
-            
-            textField.endEditing(true)
-            
-        }
-        
-    }
-    
-    //Setup Drop down
     
     func setupfirstShotDropDownMenu() {
         // The view to which the drop down will appear on
